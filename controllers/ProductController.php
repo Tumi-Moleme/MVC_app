3351560bef907f2ed5bedcff46faf5b3dd98ef90
tmln46 <t.moleme46@gmail.com>
2022-08-30 13:12:17 +0200

    First commit

---
+++ controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+class ProductController
+{
+
+  public function index()
+  {
+    echo "Index";
+  }
+  public function create()
+  {
+    echo "create";
+  }
+  public function update()
+  {
+    echo "update";
+  }
+  public function delete()
+  {
+    echo "delete";
+  }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sub-user/best_products_crud',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sub-user/best_products_crud' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
